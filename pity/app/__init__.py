a585960e7721128d59ba100ed090f78e265dc9a4
zhanzhicai2 <1610893869@qq.com>
2024-11-19 23:52:41 +0800

    编辑环境配置页面

--- pity/app/__init__.py
+++ pity/app/__init__.py
@@ -1,16 +1,51 @@
-from flask import Flask
-from flask_cors import CORS
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError, HTTPException
+from fastapi.responses import JSONResponse
+from app.excpetions.RequestException import PermissionException
+from app.excpetions.RequestException import AuthException
+
+pity = FastAPI()
 
 
-from config import Config
-
-pity = Flask(__name__)
-CORS(pity, supports_credentials=True)
-# CORS(pity)
-
-pity.config.from_object(Config)
-
-
+def error_map(error_type: str, field: str):
+    if "missing" in error_type:
+        return f"缺少参数: {field}"
+    if "params" in error_type:
+        return f"参数: {field} 不规范"
+    if "not_allowed" in error_type:
+        return f"参数: {field} 类型不正确"
 
 
+@pity.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=jsonable_encoder({
+            "code": 101,
+            "msg": error_map(exc.errors()[0]["type"], exc.errors()[0].get("loc", ['unknown'])[-1]) if len(
+                exc.errors()) > 0 else "参数解析失败",
+        })
+    )
 
+
+@pity.exception_handler(PermissionException)
+async def unexpected_exception_error(request: Request, exc: PermissionException):
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=jsonable_encoder({
+            "code": 403,
+            "msg": exc.detail,
+        })
+    )
+
+
+@pity.exception_handler(AuthException)
+async def unexpected_exception_error(request: Request, exc: AuthException):
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=jsonable_encoder({
+            "code": 401,
+            "msg": exc.detail,
+        })
+    )
--- pity/run.py
+++ pity/run.py
@@ -1,28 +1,29 @@
-from datetime import datetime
-
-from app import pity
-from app.controllers.testcase.testcase import ts
-from app.utils.logger import Log
-from app.controllers.auth.user import auth
-from app.controllers.request.http import req
-from app.controllers.project.project import pr
-from app import dao
-
-# 注册蓝图
-pity.register_blueprint(auth)
-pity.register_blueprint(req)
-pity.register_blueprint(pr)
-pity.register_blueprint(ts)
-
-
-@pity.route('/')
-def hello_world():  # put application's code here
-    log = Log("hello world专用")
-    # log.info("有人访问你的网站了")
-    now = datetime.now().strftime("%Y-%M-%d %H:%M:%S")
-    return now
-    return 'Hello World!3344'
-
-
-# if __name__ == '__main__':
-#     pity.run("0.0.0.0", threaded=True, port="7777")
+# from datetime import datetime
+#
+# from app import pity
+# from app.controllers.testcase.testcase import ts
+# from app.utils.logger import Log
+# from app.controllers.auth.user import auth
+# from app.controllers.request.http import req
+# from app.controllers.project.project import pr
+# from app import dao
+#
+#
+# # 注册蓝图
+# pity.register_blueprint(auth)
+# pity.register_blueprint(req)
+# pity.register_blueprint(pr)
+# pity.register_blueprint(ts)
+#
+#
+# @pity.route('/')
+# def hello_world():  # put application's code here
+#     log = Log("hello world专用")
+#     # log.info("有人访问你的网站了")
+#     now = datetime.now().strftime("%Y-%M-%d %H:%M:%S")
+#     return now
+#     return 'Hello World!3344'
+#
+#
+# # if __name__ == '__main__':
+# #     pity.run("0.0.0.0", threaded=True, port="7777")
